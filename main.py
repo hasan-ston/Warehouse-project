36b80b7c4b12a33041e6e8fd1716b5946730d754
maggieseto <maggieseto8@gmail.com>
2025-11-10 13:59:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
 def main():
-  print(f"Welcome, {userid}!")
-  print(authenticate(userid))
+  print(f"Welcome!")
+  userid=input("Enter your id: ")
+  authenticate(userid) #do we need this
+  while not authenticate(userid):
+    userid=input("Enter your id: ")
+    autheticate(userid)  
+
+  while not lookup_products(scan_barcode()):
+    lookup_products(scan_barcode())
+    
+    
+
+  pack_products(lookup_products(scan_barcode()))
+
+  
+    
+    
+    
+  
+  
+    
+    
+  
   print(scan(barcode))
   print
+
 
